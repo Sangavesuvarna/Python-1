8896e2b00e1c3d3039c9797b0d29b069aff0ada8
Sangavesuvarna <105164505+Sangavesuvarna@users.noreply.github.com>
2022-05-08 22:26:13 +0530

    Day 1

---
+++ Day 1/exercise4.py
@@ -0,0 +1,38 @@
+'''Write a program that switches the values stored in the variables a and b.'''
+# a = int( input("value of a"))
+# b = int(input("value of b"))
+
+a, b =  map(int ,input().split(","))
+a,b = b,a
+
+print(type(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a, b = map(int, input().split(" "))
+# a, b = b, a
+# print(a, b)
